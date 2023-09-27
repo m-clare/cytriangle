7955fd3af1f5e4b3bde0b6aeaf5ebfa05cbee263
Maryanne Wachter <maryanne.wachter@gmail.com>
2023-09-26 21:56:13 -0700

    Blacken available code

--- build_ext.py
+++ build_ext.py
@@ -5,28 +5,29 @@ from Cython.Build import cythonize
 import numpy
 
 define_macros = [
-    ('VOID', 'void'),
-    ('REAL', 'double'),
-    ('NO_TIMER', 1),
-    ('TRILIBRARY', 1),
-    ('ANSI_DECLARATORS', 1),
+    ("VOID", "void"),
+    ("REAL", "double"),
+    ("NO_TIMER", 1),
+    ("TRILIBRARY", 1),
+    ("ANSI_DECLARATORS", 1),
 ]
 
 # Extension modules
 extensions = [
     Extension(
-        'cytriangle.cytriangleio',
-        sources=['cytriangle/cytriangleio.c', 'c/triangle.c'],
-        include_dirs=['c'],
-        define_macros=define_macros
+        "cytriangle.cytriangleio",
+        sources=["cytriangle/cytriangleio.c", "c/triangle.c"],
+        include_dirs=["c"],
+        define_macros=define_macros,
     ),
     Extension(
-        'cytriangle.cytriangle',
-        sources=['cytriangle/cytriangle.c', 'c/triangle.c'],
-        include_dirs=['c'],
+        "cytriangle.cytriangle",
+        sources=["cytriangle/cytriangle.c", "c/triangle.c"],
+        include_dirs=["c"],
         define_macros=define_macros,
     ),
 ]
+
 
 class BuildExt(build_ext):
     def build_extensions(self):
@@ -35,12 +36,13 @@ class BuildExt(build_ext):
         except Exception:
             pass
 
+
 def build(setup_kwargs):
     setup_kwargs.update(
         dict(
             cmdclass=dict(build_ext=BuildExt),
-            packages=['cytriangle'],
-            ext_modules = cythonize(extensions, language_level=3),
-            zip_safe=False
+            packages=["cytriangle"],
+            ext_modules=cythonize(extensions, language_level=3),
+            zip_safe=False,
         )
     )
--- test_cytriangle/sample.py
+++ test_cytriangle/sample.py
@@ -1,11 +1,11 @@
 from cytriangle import CyTriangle
 
-sample_dict = {'point_list': [[0, 0], [0, 1], [1, 1], [1, 0]]}
+sample_dict = {"point_list": [[0, 0], [0, 1], [1, 1], [1, 0]]}
 
 if __name__ == "__main__":
     test = CyTriangle(input_dict=sample_dict)
     in_test = test.get_input_as_dict()
-    test.triangulate('a0.2')
+    test.triangulate("a0.2")
     out_test = test.get_output_as_dict()
     print(in_test)
     print(out_test)
