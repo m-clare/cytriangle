21c0ff1cafe209876f7a9ee14c24de9ffe361cd5
Maryanne Wachter <maryanne.wachter@gmail.com>
2023-09-26 21:09:44 -0700

    Swap to poetry for build system

---
+++ build_ext.py
@@ -0,0 +1,45 @@
+import setuptools
+from setuptools.extension import Extension
+from Cython.Distutils import build_ext
+from Cython.Build import cythonize
+import numpy
+
+define_macros = [
+    ('VOID', 'void'),
+    ('REAL', 'double'),
+    ('NO_TIMER', 1),
+    ('TRILIBRARY', 1),
+    ('ANSI_DECLARATORS', 1),
+]
+
+# Extension modules
+extensions = [
+    Extension(
+        'cytriangle.cytriangleio',
+        sources=['cytriangle/cytriangleio.c', 'c/triangle.c'],
+        include_dirs=['c'],
+        define_macros=define_macros
+    ),
+    Extension(
+        'cytriangle.cytriangle',
+        sources=['cytriangle/cytriangle.c', 'c/triangle.c'],
+        include_dirs=['c'],
+        define_macros=define_macros,
+    ),
+]
+
+class BuildExt(build_ext):
+    def build_extensions(self):
+        try:
+            super().build_extensions()
+        except Exception:
+            pass
+
+def build(setup_kwargs):
+    setup_kwargs.update(
+        dict(
+            cmdclas=dict(build_ext=BuildExt),
+            ext_modules = cythonize(extensions, language_level=3),
+            zip_safe=False
+        )
+    )
