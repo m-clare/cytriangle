8e0c0d182e1721aea893d9824938a0f11cbca54b
Maryanne Wachter <maryanne.wachter@gmail.com>
2023-09-30 20:19:09 -0700

    Run black

--- setup.py
+++ setup.py
@@ -2,37 +2,37 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 
 define_macros = [
-    ('VOID', 'void'),
-    ('REAL', 'double'),
-    ('NO_TIMER', 1),
-    ('TRILIBRARY', 1),
-    ('ANSI_DECLARATORS', 1),
+    ("VOID", "void"),
+    ("REAL", "double"),
+    ("NO_TIMER", 1),
+    ("TRILIBRARY", 1),
+    ("ANSI_DECLARATORS", 1),
 ]
 
 # Extension modules
 extensions = [
     Extension(
-        'cytriangle.cytriangleio',
-        sources=['cytriangle/cytriangleio.pyx', 'cpp/triangle.cpp'],
-        include_dirs=['cpp'],
-        define_macros=define_macros
+        "cytriangle.cytriangleio",
+        sources=["cytriangle/cytriangleio.pyx", "cpp/triangle.cpp"],
+        include_dirs=["cpp"],
+        define_macros=define_macros,
     ),
     Extension(
-        'cytriangle.cytriangle',
-        sources=['cytriangle/cytriangle.pyx', 'cpp/triangle.cpp'],
-        include_dirs=['cpp'],
+        "cytriangle.cytriangle",
+        sources=["cytriangle/cytriangle.pyx", "cpp/triangle.cpp"],
+        include_dirs=["cpp"],
         define_macros=define_macros,
     ),
 ]
 
-packages = ['cytriangle']
+packages = ["cytriangle"]
 
 # Define the setup
 setup(
-    name='cytriangle',
-    version='0.0.1',
-    author='Maryanne Wachter',
-    author_email='mclare@utsv.net',
+    name="cytriangle",
+    version="0.0.1",
+    author="Maryanne Wachter",
+    author_email="mclare@utsv.net",
     description="Object oriented Cython wrapper of Shewchuk's C Triangle Library",
     packages=packages,
     language="c++",
